39fa1fba8574b405c93a0513b065b0177b40543d
sangam <sangamneupane12@gmail.com>
2020-01-08 02:28:33 +0900

    added files and linked to the containers in home@ageonPressed: ()

--- lib/widgets_ui/homepage.dart
+++ lib/widgets_ui/homepage.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'hotels_ui.dart';
+import 'local_shopes_ui.dart';
+import 'nearby_events_ui.dart';
+import 'resturants_ui.dart';
+import 'tourists_locations_ui.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -35,134 +41,166 @@ class HomePage extends StatelessWidget {
                   ],
                 ),
               ),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  child: Text(
-                    'Hotels',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                ),
-                width: 280.0,
-                height: 65.0,
-                decoration: BoxDecoration(
-                  color: Color(0xFF0C96E4),
-                  borderRadius: BorderRadius.circular(20.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      offset: Offset(0.0, 3.0),
-                      blurRadius: 5.0,
+              FlatButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Hotels()));
+                },
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Text(
+                      'Hotels',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
-                  ],
+                  ),
+                  width: 280.0,
+                  height: 65.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF0C96E4),
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0.0, 3.0),
+                        blurRadius: 5.0,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  child: Text(
-                    'Resturants',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                ),
-                width: 280.0,
-                height: 65.0,
-                decoration: BoxDecoration(
-                  color: Color(0xFF0C96E4),
-                  borderRadius: BorderRadius.circular(20.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      offset: Offset(0.0, 3.0),
-                      blurRadius: 5.0,
+              FlatButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Resturants()));
+                },
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Text(
+                      'Resturants',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
-                  ],
+                  ),
+                  width: 280.0,
+                  height: 65.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF0C96E4),
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0.0, 3.0),
+                        blurRadius: 5.0,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  child: Text(
-                    'Local Shops',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                ),
-                width: 280.0,
-                height: 65.0,
-                decoration: BoxDecoration(
-                  color: Color(0xFF0C96E4),
-                  borderRadius: BorderRadius.circular(20.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      offset: Offset(0.0, 3.0),
-                      blurRadius: 5.0,
+              FlatButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LocalShops()));
+                },
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Text(
+                      'Local Shops',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
-                  ],
+                  ),
+                  width: 280.0,
+                  height: 65.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF0C96E4),
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0.0, 3.0),
+                        blurRadius: 5.0,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  child: Text(
-                    'Tourists Locations',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                ),
-                width: 280.0,
-                height: 65.0,
-                decoration: BoxDecoration(
-                  color: Color(0xFF0C96E4),
-                  borderRadius: BorderRadius.circular(20.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      offset: Offset(0.0, 3.0),
-                      blurRadius: 5.0,
+              FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => TouristsLocations()));
+                },
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Text(
+                      'Tourists Locations',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
-                  ],
+                  ),
+                  width: 280.0,
+                  height: 65.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF0C96E4),
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0.0, 3.0),
+                        blurRadius: 5.0,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  child: Text(
-                    'Nearby Events',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                ),
-                width: 280.0,
-                height: 65.0,
-                decoration: BoxDecoration(
-                  color: Color(0xFF0C96E4),
-                  borderRadius: BorderRadius.circular(20.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      offset: Offset(0.0, 3.0),
-                      blurRadius: 5.0,
+              FlatButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => NearbyEvents()));
+                },
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Text(
+                      'Nearby Events',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
-                  ],
+                  ),
+                  width: 280.0,
+                  height: 65.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF0C96E4),
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0.0, 3.0),
+                        blurRadius: 5.0,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
---
+++ lib/widgets_ui/nearby_events_ui.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NearbyEvents extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
